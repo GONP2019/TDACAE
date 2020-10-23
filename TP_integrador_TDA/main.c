bce2033caa4fde1da74ac8c94ec6b069fd5810ed
CleVered <55355215+GONP2019@users.noreply.github.com>
2020-10-23 17:51:00 -0300

    Update main.c
    Eliminación del comentario de prueba.

--- TP_integrador_TDA/main.c
+++ TP_integrador_TDA/main.c
@@ -18,23 +18,3 @@ int main()
 
     return 0;
 }
-/* Esto es una prueba para ver si sincroniza bien.
-int main()
-{
-    int a;
-    printf("Ingrese valor numerico \n");
-
-    scanf("%i",&a);
-    if (a >0) {
-        printf("El número %i es positivo \n",a);
-
-    } else
-    {
-        printf("El número %i es negativo \n",a);
-    }
-
-
-    return 0;
-}
-
-*/
