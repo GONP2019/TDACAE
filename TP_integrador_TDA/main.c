0b3e3a9946f6c134dd578a6e8008414749d4c3c5
Cesar German <german_lui_20@hotmail.com>
2020-10-28 18:36:16 -0300

    procesar()

--- TP_integrador_TDA/main.c
+++ TP_integrador_TDA/main.c
@@ -2,6 +2,42 @@
 #include <stdlib.h>
 #include <Libreria_TDA.h>
 
+void procesar(tLista* lImpares, tLista* lPrimos,tPila* p, tCola* q: tCola, _Bool *error)
+{
+    tDato num;
+    _Bool primo;
+    primo = true;
+    while(!cVacia(&q) && !error)
+    {
+        cSacar(&q, &num)
+        if(num.clave % 2 = 0)
+        {
+            if(!pLlena(&p))
+            {
+                pPoner(&num, &num)
+            } else {
+                error = 1;
+            }
+        } else {
+            if(!lLlena(&lImpares)
+               {
+                   lInsertarOrdenado(&lImpares, num, 'D');
+               } else {
+                    error = 1;
+               }
+        }
+        primo = esPrimo(num);
+        if(primo)
+        {
+            if(!lLlena(&lPrimos))
+            {
+                lInsertarOrdenado(&lPrimos, num, 'A'));
+            } else {
+                error = 1;
+            }
+        }
+    }
+}
  main()
 {
     printf("Hola Mundoooooooooooooooooooooooo!");
