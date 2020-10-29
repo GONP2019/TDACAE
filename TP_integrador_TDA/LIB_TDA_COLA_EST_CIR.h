fd36d02fe4e14de5767768ae186f73a20b590b7a
CleVered <55355215+GONP2019@users.noreply.github.com>
2020-10-29 19:16:39 -0300

    Implementación TDA_COLA_CIRCULAR
    He desarrollado la implementación de TDA_COLA_CIRCULAR y estoy realizando prueba de todas las funciones.

--- TP_integrador_TDA/LIB_TDA_COLA_EST_CIR.h
+++ TP_integrador_TDA/LIB_TDA_COLA_EST_CIR.h
@@ -10,5 +10,71 @@ typedef struct tCola
     int arrCC[MAX];
 }tCola;
 
+void ccCrear (tCola* k)
+{
+    k->frente = 1 ;
+    k->finc = 1;
+}
+
+int ccLlena (tCola* k)
+{
+    /* Si devuelve 1 significa que está llena */
+    int res;
+    if ((k->finc + 1 == k->frente)||(k->frente == 1 && k->finc == MAX))
+    {
+        res = 1;
+    }else
+    {
+        res = 0;
+    }
+    return res;
+}
+
+int ccVacia (tCola* k)
+{
+    int res;
+    if (k->finc == k->frente)
+    {
+        res = 1;
+    }else
+    {
+        res = 0;
+    }
+    return res;
+}
+
+void ccPoner (tCola* k,tDato dato)
+{
+    if (k->finc == MAX)
+    {
+        k->finc = 1;
+    }else
+    {
+        k->finc = k->finc + 1;
+    }
+    k->arrCC[k->finc] = dato;
+}
+
+void ccSacar (tCola* k, tDato dato)
+{
+    if (k->frente == MAX)
+    {
+        k->frente = 1;
+    }else
+    {
+        k->frente = k->frente + 1;
+    }
+    dato = k->arrCC[k->frente];
+}
+
+     /*   int res;
+        if (k.finc + 1 == k.frente) || (k.frente == 1 && k.finc == MAX)
+        {
+            res = 1;
+        }else
+        {
+            res = 0;
+        }
+        return res;*/
 
 #endif // LIB_TDA_COLA_EST_CIR
--- TP_integrador_TDA/main.c
+++ TP_integrador_TDA/main.c
@@ -5,8 +5,38 @@
 
  main()
 {
+    tDato dato;
+    tCola k;
+    int rt;
+    ccCrear(&k);
 
-    printf("Hola Mundoooooooooooooooooooooooo!");
+    /*printf("Desea ingresar datos ? S/N \n");
+    scanf ("%c",rt);*/
+
+        printf("Desea ingresar datos ? 1/0 \n");
+        scanf ("%i",&rt);
+
+    while (rt == 1 && ccLlena(&k)!= 1)
+        {
+            printf("Ingrese numero entero positivo\n");
+            scanf ("%i",&dato);
+            ccPoner(&k,dato);
+            printf("Desea ingresar datos ? 1/0 \n");
+            scanf ("%i",&rt);
+        }
+
+    if (ccLlena(&k)== 1)
+    {
+        printf("Cola llena");
+
+        for (int j=1;j<=MAX;j++){
+            printf("El numero es %i \n",k.arrCC[j]);
+        }
+    }
+    else{
+        printf("Todavia hay espacio");
+    }
+
 }
 
 /*void procesar(tLista* lImpares, tLista* lPrimos,tPila* p, tCola* q: tCola, _Bool *error)
