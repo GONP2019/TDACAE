3dbe0904fc55df805ff61fe4230409da75fa569c
CleVered <55355215+GONP2019@users.noreply.github.com>
2020-10-29 09:04:39 -0300

    Definición de constante

--- TP_integrador_TDA/LIB_TDA_COLA_EST_CIR.h
+++ TP_integrador_TDA/LIB_TDA_COLA_EST_CIR.h
@@ -1,6 +1,14 @@
 #ifndef LIB_TDA_COLA_EST_CIR
 #define LIB_TDA_COLA_EST_CIR
+#define MAX 5
+typedef int tDato;
 
+typedef struct tCola
+{
+    tDato frente;
+    tDato finc;
+    int arrCC[MAX];
+}tCola;
 
 
 #endif // LIB_TDA_COLA_EST_CIR
--- TP_integrador_TDA/main.c
+++ TP_integrador_TDA/main.c
@@ -5,6 +5,7 @@
 
  main()
 {
+
     printf("Hola Mundoooooooooooooooooooooooo!");
 }
 
